99d140abef194c57ea513c7a70df3ea8fb4b6f8b
Yogeshkodes <yogesh.cr712@gmail.com>
2025-07-03 17:15:55 +0530

    Navbar done and Clerk integrated

---
+++ Client/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  const { openSignIn } = useClerk();
+
+  const { user } = useUser();
+  return (
+    <div className="shadow">
+      <div className="flex container px-4 2xl:px-20 mx-auto justify-between items-center">
+        <img src={assets.logo} alt="" className="h-[100px] w-[180px]" />
+        {user ? (
+          <div className="flex gap-5 items-center ">
+            <Link to={"applications"}>Applied Jobs</Link>
+            <p>|</p>
+            <p>Hi , {user.firstName + " " + user.lastName}</p>
+            <UserButton />
+          </div>
+        ) : (
+          <div className="flex gap-5 max-sm:text-xs">
+            <button className="text-gray-600 cursor-pointer">
+              Recruiter Login
+            </button>
+            <button
+              onClick={() => openSignIn()}
+              className="bg-[#ffa62e] px-6 sm:px-9 py-2 text-white rounded-full cursor-pointer"
+            >
+              Login
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
---
+++ Client/src/pages/Applications.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Applications = () => {
+  return <div className="bg-zinc-800">Application</div>;
+};
