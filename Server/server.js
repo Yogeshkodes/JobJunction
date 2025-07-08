6aa9e2b15f33ca99059d733f2b5f7fd5aee8e503
Yogeshkodes <yogesh.cr712@gmail.com>
2025-07-08 20:33:18 +0530

    webhook and userController done

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import connectDB from "./config/db.js";
 import "./config/instrument.js";
 import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from "./controllers/webhook.controller.js";
 
 // initialize express app
 const app = express();
@@ -17,7 +18,7 @@ app.get("/", (req, res) => {
   res.send("Hello from server");
 });
 
-
+app.post("/webhooks", clerkWebhooks);
 // connect DB
 
 await connectDB();
