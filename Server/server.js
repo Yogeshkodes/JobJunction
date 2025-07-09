c0f4cece16b3be340da53f871d1f1d515939feab
Yogeshkodes <yogesh.cr712@gmail.com>
2025-07-09 10:16:37 +0530

    Vercel json file added

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,8 @@ import connectDB from "./config/db.js";
 import "./config/instrument.js";
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from "./controllers/webhook.controller.js";
+import User from "./models/userSchema.model.js";
+import mongoose from "mongoose";
 
 // initialize express app
 const app = express();
@@ -16,6 +18,20 @@ app.use(express.json());
 // routes
 app.get("/", (req, res) => {
   res.send("Hello from server");
+});
+
+app.get("/seed-user", async (req, res) => {
+  try {
+    const u = await User.create({
+      _id: new mongoose.Types.ObjectId(),
+      name: "Test User",
+      email: "test@example.com",
+      resume: "fdsf",
+    });
+    return res.status(201).json(u);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 });
 
 app.post("/webhooks", clerkWebhooks);
